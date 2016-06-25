d4541e676eaa14ccc4d223512c8e58b02eda2040
Anthony Bloomer <ant0@protonmail.ch>
2016-06-25 04:04:56 +0100

    Sat 25 Jun 2016 04:04:56 IST

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='zoopla',
+      version='0.1',
+      description='A simple wrapper for the Zoopla API',
+      url='https://github.com/AnthonyBloomer/zoopla',
+      author='Anthony Bloomer',
+      author_email='ant0@protonmail.ch',
+      license='MIT',
+      packages=['zoopla'],
+
+      zip_safe=False)
---
+++ zoopla/__init__.py
@@ -0,0 +1,82 @@
+import requests
+import unittest
+
+
+class Zoopla:
+    def __init__(self, api_key):
+        self.url = 'http://api.zoopla.co.uk/api/v1/'
+        self.api_key = api_key
+
+    def local_info_graphs(self, area):
+        return self._call('local_info_graphs.js', {
+            'api_key': self.api_key,
+            'area': area
+        })
+
+    def area_value_graphs(self, area, size='medium'):
+        return self._call('area_value_graphs.js?', {
+            'api_key': self.api_key,
+            'area': area,
+            'size': size
+        })
+
+    def search_property_listings(self, params):
+        params.update({'api_key': self.api_key})
+        return self._call('property_listings.json?', params)
+
+    def get_average_area_sold_price(self, area=None, postcode=None, output_type='outcode', area_type='streets'):
+        return self._call('average_area_sold_price.json?', {
+            'api_key': self.api_key,
+            'postcode': postcode,
+            'area': area,
+            'output_type': output_type,
+            'area_type': area_type
+        })
+
+    def _call(self, action, params):
+        r = requests.get(self.url + action, params)
+        if r.status_code == 200:
+            return r.json()
+        else:
+            raise ZooplaException(str(r.status_code), r.reason, r.text)
+
+
+class ZooplaException(Exception):
+    def __init__(self, status_code, reason, text):
+        self.status_code = status_code
+        self.reason = reason
+        self.text = text
+
+    def __str__(self):
+        return "Zoopla returned an error: " + str(self.status_code) + " - " + self.reason + " - " + self.text
+
+
+class ZooplaTests(unittest.TestCase):
+    def setUp(self):
+        self.zoopla = Zoopla('')
+
+    def test_area_value_graphs(self):
+        area_graphs = self.zoopla.area_value_graphs('SW11')
+        self.assertIsNotNone(area_graphs['area_name'], 'SW11')
+
+    def test_get_average_area_sold_price(self):
+        averages = self.zoopla.get_average_area_sold_price('SW11')
+        print averages
+        self.assertEquals(averages['average_sold_price_1year'], '814144')
+
+    def test_search_property_listings(self):
+        search = self.zoopla.search_property_listings(params={
+            'maximum_beds': 2,
+            'page_size': 100,
+            'listing_status': 'rent',
+            'area': 'Blackley, Greater Manchester'
+        })
+        self.assertEquals(search['county'], 'Greater Manchester')
+
+    def test_local_info_graphs(self):
+        local_graphs = self.zoopla.local_info_graphs('SW11')
+        self.assertIsNotNone(local_graphs['area_name'], 'SW11')
+
+
+if __name__ == '__main__':
+    unittest.main()
