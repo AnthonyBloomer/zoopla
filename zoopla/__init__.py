8873b37b38cc82af578312039e2b26ab1d753871
Anthony Bloomer <ant0@protonmail.ch>
2016-09-16 17:04:43 +0100

    Add get_session_id method

--- zoopla/__init__.py
+++ zoopla/__init__.py
@@ -1,5 +1,6 @@
 import requests
 
+
 class Object:
     def __init__(self, **entries):
         self.__dict__.update(entries)
@@ -9,22 +10,29 @@ class Zoopla:
     def __init__(self, api_key):
         self.url = 'http://api.zoopla.co.uk/api/v1/'
         self.api_key = api_key
-    
+
     def local_info_graphs(self, area):
         return Object(**self._call('local_info_graphs.js', {
             'api_key': self.api_key,
             'area': area
         }))
-    
+
+    def get_session_id(self):
+        response = self._call('get_session_id.json?', {
+            'api_key': self.api_key
+        })
+
+        return response['session_id']
+
     def zed_index(self, area, output_type='outcode'):
-        return Object(**self._call('zed_index.js?', {
+        return Object(**self._call('zed_index.json?', {
             'api_key': self.api_key,
             'area': area,
             'output_type': output_type
         }))
 
     def area_value_graphs(self, area, size='medium'):
-        return Object(**self._call('area_value_graphs.js?', {
+        return Object(**self._call('area_value_graphs.json?', {
             'api_key': self.api_key,
             'area': area,
             'size': size
@@ -36,7 +44,7 @@ class Zoopla:
         result = []
         [result.append(Object(**r)) for r in c['listing']]
         return result
-    
+
     def get_average_area_sold_price(self, area=None, postcode=None, output_type='outcode', area_type='streets'):
         return Object(**self._call('average_area_sold_price.json?', {
             'api_key': self.api_key,
@@ -45,15 +53,16 @@ class Zoopla:
             'output_type': output_type,
             'area_type': area_type
         }))
-    
+
     def auto_complete(self, search_term, search_type='properties'):
         return Object(**self._call('geo_autocomplete.json?', {
             'api_key': self.api_key,
             'search_term': search_term,
             'search_type': search_type
         }))
-    
-    def area_zed_indices(self, area, area_type='streets', output_type='area', order='ascending', page_number=1, page_size=10):
+
+    def area_zed_indices(self, area, area_type='streets', output_type='area', order='ascending', page_number=1,
+                         page_size=10):
         return Object(**self._call('zed_indices.json', {
             'api_key': self.api_key,
             'area': area,
@@ -64,10 +73,11 @@ class Zoopla:
             'page_size': page_size
 
         }))
-        
+
     def _call(self, action, params):
         r = requests.get(self.url + action, params)
         if r.status_code == 200:
+            print r.json()
             return r.json()
         else:
             raise ZooplaException(str(r.status_code), r.reason, r.text)
@@ -81,4 +91,3 @@ class ZooplaException(Exception):
 
     def __str__(self):
         return "Zoopla returned an error: " + str(self.status_code) + " - " + self.reason + " - " + self.text
-
